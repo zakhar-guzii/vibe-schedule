1ee1c94345aa0a13a97598fa05a0c2610ad49ab5
Zakhar Guzii <zakharguzii@gmail.com>
2025-06-28 21:16:24 +0300

    add config

--- src/schedule_algo.py
+++ src/schedule_algo.py
@@ -1,22 +1,8 @@
+import pandas as pd
+import networkx as nx
 from datetime import datetime, time, timedelta
 from src.load_data import prep_data
-
-classroom = ['1.08.1', '1.08.1', '1.15.1', '1.15.2',
-             '2.08.1', '2.08.2', '2.15.1', '2.15.2',
-             '3.05', '4.05', '4.08', '4.16',
-             'Феофанія 1', 'Феофанія 2', 'Феофанія 3', 'Феофанія 4']
-
-
-def time_for_lessons():
-    start0 = datetime.combine(datetime.today(), time(8, 30))
-    pair_len = timedelta(minutes=80)
-    breaks = {1: 10, 2: 10, 3: 10, 4: 40, 5: 10, 6: 10, 7: 10}
-    slot_time = {}
-    cur = start0
-    for p in range(1, 9):
-        slot_time[p] = (cur.time(), (cur + pair_len).time())
-        cur = cur + pair_len + timedelta(minutes=breaks.get(p))
-    return slot_time
+from config import CLASSROOMS, time_for_lesson
 
 
 def main():
