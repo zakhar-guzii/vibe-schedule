622be7b081128d4ba08aaaecbb7a66555588140e
Zakhar Guzii <zakharguzii@gmail.com>
2025-06-28 20:44:53 +0300

    add based structure

---
+++ src/load_data.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+
+def prep_data():
+    file = 'Розклад для студентів (літній терм 2025).xlsx'
+    df_groups = pd.read_excel(file, sheet_name='Розподіл на групи')
+    df_schedule = pd.read_excel(file, sheet_name='Розклад')
+    df_courses = pd.read_excel(file, sheet_name='Дисципліни')
+
+    fixed_cols = ['Прізвище', "Ім'я"]
+    discipline_cols = [c for c in df_groups.columns if c not in fixed_cols]
+
+    df_long = df_groups.melt(
+        id_vars=fixed_cols,
+        value_vars=discipline_cols,
+        var_name='Дисципліна',
+        value_name='Група'
+    ).dropna(subset=['Група'])
+
+    df_final = df_courses.copy()
+
+    def get_students(disc, grp):
+        mask = (df_long['Дисципліна'] == disc) & (df_long['Група'] == grp)
+        return df_long.loc[mask, 'ПІБ'].tolist()
+
+    df_final['Студенти'] = df_final.apply(
+        lambda r: get_students(r['Дисципліна'], r['Група']), axis=1
+    )
+
+
